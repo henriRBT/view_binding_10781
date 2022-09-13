06aadc092ae070e5fa588b2e10aa51ee6b6dd280
Henri <henritom1301@gmail.com>
2022-09-13 14:24:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10781/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.view_binding_10781
+
+object TaskList {
+    val tasklist = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur",
+        "Kebiasaan  sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Ngopi",
+            "Mencari Primo buat Gatcha Bom Bom Bakudan"),
+        Task("Chapter 3", "Bangun Tidur",
+            "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Ei can't cooking",
+            "Buat aduk Mekanik Bom Bakudan klee"),
+        Task("Chapter 5", "Upload ke Tiktok",
+            "Buat Pamer ke view Bom Bakudan Klee C6 + Lisa C6"),
+
+    )
+}
